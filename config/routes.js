83f4a4ebba233d24b3831788a1287076c4f27f46
Sam Stiles <sam@phasesolutions.ca>
2015-02-02 20:01:03 -0400

    Routes

--- config/routes.js
+++ config/routes.js
@@ -1,1 +1,95 @@
-module.exports.routes = {};
+module.exports.routes = {
+
+  /**
+   * GET (read) routes by Slug
+   */
+  'GET /api/climb/findBySlug/:slug': {
+    controller: 'ClimbController',
+    action: 'findBySlug'
+  },
+
+  'GET /api/location/findBySlug/:slug': {
+    controller: 'LocationController',
+    action: 'findBySlug'
+  },
+
+  'GET /api/sublocation/findBySlug/:slug': {
+    controller: 'SublocationController',
+    action: 'findBySlug'
+  },
+
+  'GET /api/user/findBySlug/:slug': {
+    controller: 'UserController',
+    action: 'findBySlug'
+  },
+
+  /**
+   * GET (read) routes by Id
+   */
+  'GET /api/climb/findById/:slug': {
+    controller: 'ClimbController',
+    action: 'findById'
+  },
+
+  'GET /api/location/findById/:slug': {
+    controller: 'LocationController',
+    action: 'findById'
+  },
+
+  'GET /api/sublocation/findById/:slug': {
+    controller: 'SublocationController',
+    action: 'findById'
+  },
+
+  'GET /api/user/findById/:slug': {
+    controller: 'UserController',
+    action: 'findById'
+  },
+
+  /**
+   * POST (create) routes
+   */
+  'POST /api/climb': {
+    controller: 'ClimbController',
+    action: 'create'
+  },
+
+  'POST /api/location': {
+    controller: 'LocationController',
+    action: 'create'
+  },
+
+  'POST /api/sublocation': {
+    controller: 'SublocationController',
+    action: 'create'
+  },
+
+  'POST /api/user': {
+    controller: 'UserController',
+    action: 'create'
+  },
+
+  /**
+   * PUT (update) routes
+   */
+  'PUT /api/climb': {
+    controller: 'ClimbController',
+    action: 'update'
+  },
+
+  'PUT /api/location': {
+    controller: 'LocationController',
+    action: 'update'
+  },
+
+  'PUT /api/sublocation': {
+    controller: 'SublocationController',
+    action: 'update'
+  },
+
+  'PUT /api/user': {
+    controller: 'UserController',
+    action: 'update'
+  }
+
+};
