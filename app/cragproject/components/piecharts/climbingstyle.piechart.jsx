65de2db3d7db72513925d98d9dcc0648dbbaf4c6
Sam Stiles <sam@phasesolutions.ca>
2015-02-06 23:10:48 -0400

    Charts are hard.

--- app/cragproject/components/piecharts/climbingstyle.piechart.jsx
+++ app/cragproject/components/piecharts/climbingstyle.piechart.jsx
@@ -24,40 +24,46 @@ var ClimbingStylePieChart = React.createClass({
   componentDidMount: function() {
     this.state.counts = [
       {
-        value: _.countBy(this.state.climbs, function(climb) { return climb.style === 'Traditional' }),
+        value: _.countBy(this.state.climbs, function(climb) { return climb.style === 'Traditional' }).true,
         color: 'rgba(247, 70, 74, 1)',
         highlight: 'rgba(247, 70, 74, 0.75)',
         label: 'Traditional'
       },
       {
-        value: _.countBy(this.state.climbs, function(climb) { return climb.style === 'Sport' }),
+        value: _.countBy(this.state.climbs, function(climb) { return climb.style === 'Sport' }).true,
         color: 'rgba(70, 191, 189, 1)',
         highlight: '#rgba(70, 191, 189, 0.75)',
         label: 'Sport'
       },
       {
-        value: _.countBy(this.state.climbs, function(climb) { return climb.style === 'Aid' }),
+        value: _.countBy(this.state.climbs, function(climb) { return climb.style === 'Aid' }).true,
         color: 'rgba(253, 180, 92, 1)',
         highlight: 'rgba(253, 180, 92, 0.75)',
         label: 'Aid'
       },
       {
-        value: _.countBy(this.state.climbs, function(climb) { return climb.style === 'Mixed' }),
+        value: _.countBy(this.state.climbs, function(climb) { return climb.style === 'Mixed' }).true,
         color: 'rgba(220, 220, 220, 1)',
         highlight: 'rgba(220, 220, 220, 0.75)',
         label: 'Mixed'
       },
       {
-        value: _.countBy(this.state.climbs, function(climb) { return climb.style === 'Boulder' })
+          value: _.countBy(this.state.climbs, function(climb) { return climb.style === 'Boulder' }).true,
           color: 'rgba(151, 187, 205, 1)',
           highlight: 'rgba(151, 187, 205, 0.75)',
           label: 'Boulder'
       }
     ];
+    var ctx = this.refs.chartCanvas.getDOMNode();
+    var chart = new Chart(ctx).Pie(this.state.counts, this.state.options);
   },
 
   render: function() {
-    return <PieChart data={this.state.counts} options={this.state.options} />
+    return (
+      <div>
+        <canvas ref="chartCanvas" height="200" width="200" />
+      </div>
+    );
   }
 
 });
