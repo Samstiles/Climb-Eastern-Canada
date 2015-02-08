35fd444bb90c219abc7b65eb3055ff066e77af62
Sam Stiles <sam@phasesolutions.ca>
2015-02-08 16:58:04 -0400

    Default theme for now, and register/login routes

---
+++ app/cragproject/states/register.js
@@ -0,0 +1,16 @@
+CragProject.config(['$stateProvider',
+  function($stateProvider) {
+
+    $stateProvider.state('register', {
+      url: '/register',
+      templateUrl: '/cragproject/states/register.template.html',
+      controllerAs: 'RegisterCtrl',
+      controller: ['UserModel', function(UserModel) {
+        var ctrl = this;
+        ctrl.user = new UserModel();
+        window.RegisterController = ctrl;
+      }]
+    });
+
+  }
+]);
